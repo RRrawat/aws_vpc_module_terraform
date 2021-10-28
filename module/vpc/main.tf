4724ed073328582d5891ca82416622f3d8eabb95
RRrawat <81628422+RRrawat@users.noreply.github.com>
2021-10-28 14:11:00 +0530

    Update main.tf

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  access_key = var.AWS_ACCESS_KEY
-  secret_key = var.AWS_SECRET_KEY
-  region     = us-east-1
+  access_key = "${var.AWS_ACCESS_KEY}
+  secret_key = "${var.AWS_SECRET_KEY}
+  region     = "us-east-1"
 }
 
 
