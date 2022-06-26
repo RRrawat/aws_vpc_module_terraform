9f97b276d9a2756d9610eab22a5281c045cb916e
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-06-26 12:25:08 +0530

    Create eip.tf

---
+++ eip.tf
@@ -0,0 +1,31 @@
+locals {
+  max_subnet_length = max(
+    length(var.private_subnets)
+  )
+  nat_gateway_count = var.single_nat_gateway ? 1 : var.one_nat_gateway_per_az ? length(var.azs) : local.max_subnet_length
+
+  # Use `local.vpc_id` to give a hint to Terraform that subnets should be deleted before secondary CIDR blocks can be free!
+  vpc_id = element(
+    concat(
+      aws_vpc_ipv4_cidr_block_association.this.*.vpc_id,
+      aws_vpc.this.*.id,
+      [""],
+    ),
+    0,
+  )
+}
+
+locals {
+  nat_gateway_ips = split(
+    ",",
+    var.reuse_nat_ips ? join(",", var.external_nat_ip_ids) : join(",", aws_eip.nat.*.id),
+  )
+}
+
+resource "aws_eip" "nat" {
+  count = var.create_vpc && var.enable_nat_gateway && false == var.reuse_nat_ips ? local.nat_gateway_count : 0
+
+  vpc = true
+
+  tags = module.tags.commontags
+}
