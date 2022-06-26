0196e92e7825b38aea2dc56b16af5b90428170d8
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-06-26 12:22:59 +0530

    Update route.tf

--- route.tf
+++ route.tf
@@ -29,3 +29,23 @@ resource "aws_route" "private_nat_gateway" {
   }
 }
 
+################################################################################
+# Route table association
+################################################################################
+
+resource "aws_route_table_association" "private" {
+  count = var.create_vpc && length(var.private_subnets) > 0 ? length(var.private_subnets) : 0
+
+  subnet_id = element(aws_subnet.private.*.id, count.index)
+  route_table_id = element(
+    aws_route_table.private.*.id,
+    var.single_nat_gateway ? 0 : count.index,
+  )
+}
+
+resource "aws_route_table_association" "public" {
+  count = var.create_vpc && length(var.public_subnets) > 0 ? length(var.public_subnets) : 0
+
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public[0].id
+}
