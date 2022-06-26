858ff82c4c11888f673dc79d0d8fcb7bcc906058
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-06-26 12:14:42 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,24 @@
-resource "aws_vpc" "vpc_test" {
-  cidr_block              = "${var.vpc-cidr}"
-  instance_tenancy        = "default"
-  enable_dns_hostnames    = true #gives you an internal host name
+################################################################################
+# VPC
+################################################################################
+resource "aws_vpc" "this" {
+  count = var.create_vpc ? 1 : 0
 
-  tags      = {
-    Name    = "${var.vpc_name}"
-  }
+  cidr_block                       = var.cidr
+  instance_tenancy                 = var.instance_tenancy
+  enable_dns_hostnames             = var.enable_dns_hostnames
+  enable_dns_support               = var.enable_dns_support
+
+  tags = module.tags.commontags
+}
+
+
+#comman tags
+module "tags" {
+    source = "../../../common/common_tags"
+
+    stack = var.stack
+    name = var.name
+    enviroment = var.enviroment
+    additional_tags = var.additional_tags
 }
