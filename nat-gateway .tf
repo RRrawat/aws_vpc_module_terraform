f66e597c231fa0805844c96fe036388089425a1b
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-06-26 12:19:43 +0530

    Update nat-gateway .tf

--- nat-gateway .tf
+++ nat-gateway .tf
@@ -1,1 +1,17 @@
 
+resource "aws_nat_gateway" "this" {
+  count = var.create_vpc && var.enable_nat_gateway ? local.nat_gateway_count : 0
+
+  allocation_id = element(
+    local.nat_gateway_ips,
+    var.single_nat_gateway ? 0 : count.index,
+  )
+  subnet_id = element(
+    aws_subnet.public.*.id,
+    var.single_nat_gateway ? 0 : count.index,
+  )
+
+  tags = module.tags.commontags
+
+  depends_on = [aws_internet_gateway.this]
+}
