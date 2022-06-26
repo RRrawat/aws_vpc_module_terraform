1f42ca7eb5aa8bde03aaaa21a7c8e7276ff986bf
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-06-26 12:24:43 +0530

    Create internet-gateway.tf

---
+++ internet-gateway.tf
@@ -0,0 +1,11 @@
+################################################################################
+# Internet Gateway
+################################################################################
+
+resource "aws_internet_gateway" "this" {
+  count = var.create_vpc && var.create_igw && length(var.public_subnets) > 0 ? 1 : 0
+
+  vpc_id = local.vpc_id
+
+  tags = module.tags.commontags
+}
