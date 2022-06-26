abdc90fe0413f0c3226e93d9a5459bc1b028d017
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-06-26 12:23:36 +0530

    Update route.tf

--- route.tf
+++ route.tf
@@ -1,5 +1,31 @@
 
 ################################################################################
+# Publiс routes
+################################################################################
+
+resource "aws_route_table" "public" {
+  count = var.create_vpc && length(var.public_subnets) > 0 ? 1 : 0
+
+  vpc_id = local.vpc_id
+
+  tags = module.tags.commontags
+}
+
+################################################################################
+# Private routes
+# There are as many routing tables as the number of NAT gateways
+################################################################################
+
+resource "aws_route_table" "private" {
+  count = var.create_vpc && local.max_subnet_length > 0 ? local.nat_gateway_count : 0
+
+  vpc_id = local.vpc_id
+
+  tags = module.tags.commontags
+    
+}
+  
+################################################################################
 # Internet Gateway
 ################################################################################
 resource "aws_route" "public_internet_gateway" {
