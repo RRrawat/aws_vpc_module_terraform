28ea993821b3dda02b36bff781563b35080e22bc
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-06-26 12:26:02 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -22,3 +22,12 @@ module "tags" {
     enviroment = var.enviroment
     additional_tags = var.additional_tags
 }
+
+  
+resource "aws_vpc_ipv4_cidr_block_association" "this" {
+  count = var.create_vpc && length(var.secondary_cidr_blocks) > 0 ? length(var.secondary_cidr_blocks) : 0
+
+  vpc_id = aws_vpc.this[0].id
+
+  cidr_block = element(var.secondary_cidr_blocks, count.index)
+}
