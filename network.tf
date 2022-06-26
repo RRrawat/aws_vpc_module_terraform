1d92548ddc2e5d428c17e7703898c1ca710a2966
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-06-26 12:19:02 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,4 +1,30 @@
 
+################################################################################
+# Public Network ACLs
+################################################################################
+
+resource "aws_network_acl" "public" {
+  count = var.create_vpc && length(var.public_subnets) > 0 ? 1 : 0
+
+  vpc_id     = element(concat(aws_vpc.this.*.id, [""]), 0)
+  subnet_ids = aws_subnet.public.*.id
+
+  tags = module.tags.commontags
+}
+
+################################################################################
+# Private Network ACLs
+################################################################################
+
+resource "aws_network_acl" "private" {
+  count = var.create_vpc && length(var.private_subnets) > 0 ? 1 : 0
+
+  vpc_id     = element(concat(aws_vpc.this.*.id, [""]), 0)
+  subnet_ids = aws_subnet.private.*.id
+
+  tags = module.tags.commontags
+}
+
 #public
 resource "aws_network_acl_rule" "public_inbound" {
   count = var.create_vpc && length(var.public_subnets) > 0 ? length(var.public_inbound_acl_rules) : 0
