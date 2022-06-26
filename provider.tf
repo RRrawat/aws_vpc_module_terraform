6df324652c361c859f559518cef55eaf5e54aeba
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-06-26 12:21:06 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,3 +2,14 @@ provider "aws" {
   region  = var.region
   profile = var.aws_profile
 }
+
+terraform {
+  required_version = ">= 0.13.1"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.63"
+    }
+  }
+}
