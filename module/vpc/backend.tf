40bdeffecc9e0a15423bba5332cec1c57b671c80
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-06-27 12:18:45 +0530

    Update backend.tf

--- module/vpc/backend.tf
+++ module/vpc/backend.tf
@@ -1,1 +1,9 @@
-
+terraform {
+  backend "s3" {
+    profile = "default"
+    bucket = "terraform-state-files9"
+    key    = "vpcstate/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-state"
+  }
+}
