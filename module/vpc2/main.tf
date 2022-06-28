e76f3d50638cd1173522c555d92eb66a5854c357
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-06-28 17:50:16 +0530

    Delete main.tf

--- module/vpc2/main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = "us-east-1"
-  AWS_ACCESS_KEY = var.AWS_ACCESS_KEY
-  AWS_SECRET_KEY = var.AWS_SECRET_KEY
-}
-
-resource "aws_vpc" "vpc_test" {
-  cidr_block              = "10.0.0.0/16"
-}
