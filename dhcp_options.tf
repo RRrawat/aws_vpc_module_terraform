3ecf8c6032ac0c0a391b53b418e352755ebf0d50
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-08-01 19:30:23 +0530

    Update dhcp_options.tf

--- dhcp_options.tf
+++ dhcp_options.tf
@@ -1,1 +1,28 @@
 
+################################################################################
+# DHCP Options Set
+################################################################################
+
+resource "aws_vpc_dhcp_options" "this" {
+  count = local.create_vpc && var.enable_dhcp_options ? 1 : 0
+
+  domain_name          = var.dhcp_options_domain_name
+  domain_name_servers  = var.dhcp_options_domain_name_servers
+  ntp_servers          = var.dhcp_options_ntp_servers
+  netbios_name_servers = var.dhcp_options_netbios_name_servers
+  netbios_node_type    = var.dhcp_options_netbios_node_type
+
+  tags = merge(
+    { "Name" = var.name },
+    var.tags,
+    var.dhcp_options_tags,
+  )
+}
+
+resource "aws_vpc_dhcp_options_association" "this" {
+  count = local.create_vpc && var.enable_dhcp_options ? 1 : 0
+
+  vpc_id          = local.vpc_id
+  dhcp_options_id = aws_vpc_dhcp_options.this[0].id
+}
+
